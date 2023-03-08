cf6daa4a85444d16b279a31e31041c0525587d32
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-03-08 09:25:36 +0100

    Add base entities

---
+++ src/Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Common
+{
+    public abstract class BaseAuditableEntity<T> : BaseEntity<T>
+    {
+        public DateTime Created { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+}
