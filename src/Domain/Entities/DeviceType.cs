1a1cdf6bae5619995bb3bf15d10c6f11162e6db2
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-03-09 09:02:25 +0100

    Add entities

---
+++ src/Domain/Entities/DeviceType.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public sealed class DeviceType : BaseEntity<int>
+    {
+        public string? Name { get; set; }
+        public DeviceType? Parent { get; set; }
+        public int? ParentId { get; set; }
+        public ICollection<Device>? Devices { get; set; }
+        public ICollection<DeviceTypeProperty>? DeviceTypeProperties { get; set; }
+    }
+}
