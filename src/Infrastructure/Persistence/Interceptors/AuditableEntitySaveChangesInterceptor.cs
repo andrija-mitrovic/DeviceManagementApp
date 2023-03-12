517b126f5cabf297b40e65e00ae701cbd5d87041
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-03-12 08:41:16 +0100

    Add interceptor

---
+++ src/Infrastructure/Persistence/Interceptors/AuditableEntitySaveChangesInterceptor.cs
@@ -0,0 +1,56 @@
+﻿using Application.Common.Interfaces;
+using Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Infrastructure.Persistence.Interceptors
+{
+    public sealed class AuditableEntitySaveChangesInterceptor : SaveChangesInterceptor
+    {
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IDateTime _dateTime;
+
+        public AuditableEntitySaveChangesInterceptor(
+            ICurrentUserService currentUserService,
+            IDateTime dateTime)
+        {
+            _currentUserService = currentUserService;
+            _dateTime = dateTime;
+        }
+
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            UpdateEntities<BaseAuditableEntity<int>>(eventData.Context);
+
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            UpdateEntities<BaseAuditableEntity<int>>(eventData.Context);
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        public void UpdateEntities<T>(DbContext? context)
+        {
+            if (context == null) return;
+
+            foreach (var entry in context.ChangeTracker.Entries<BaseAuditableEntity<T>>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedBy = _currentUserService.UserId;
+                    entry.Entity.Created = _dateTime.Now;
+                }
+
+                if (entry.State == EntityState.Added || entry.State == EntityState.Modified || entry.HasChangedOwnedEntities())
+                {
+                    entry.Entity.LastModifiedBy = _currentUserService.UserId;
+                    entry.Entity.LastModified = _dateTime.Now;
+                }
+            }
+        }
+    }
+}
