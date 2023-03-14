98ba8b77b575b94ffa23638b80b6f14019030b40
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-03-14 08:41:59 +0100

    Add initial migration

---
+++ src/Infrastructure/Persistence/Migrations/20230312151207_InitialCreate.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeviceTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ParentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceTypes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeviceTypes_DeviceTypes_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "DeviceTypes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    DeviceTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Devices_DeviceTypes_DeviceTypeId",
+                        column: x => x.DeviceTypeId,
+                        principalTable: "DeviceTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeviceTypeProperties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    DeviceTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceTypeProperties", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeviceTypeProperties_DeviceTypes_DeviceTypeId",
+                        column: x => x.DeviceTypeId,
+                        principalTable: "DeviceTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DevicePropertyValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeviceTypePropertyId = table.Column<int>(type: "int", nullable: false),
+                    DeviceId = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DevicePropertyValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DevicePropertyValues_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DevicePropertyValues_DeviceTypeProperties_DeviceTypePropertyId",
+                        column: x => x.DeviceTypePropertyId,
+                        principalTable: "DeviceTypeProperties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DevicePropertyValues_DeviceId",
+                table: "DevicePropertyValues",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DevicePropertyValues_DeviceTypePropertyId",
+                table: "DevicePropertyValues",
+                column: "DeviceTypePropertyId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_DeviceTypeId",
+                table: "Devices",
+                column: "DeviceTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceTypeProperties_DeviceTypeId_Name",
+                table: "DeviceTypeProperties",
+                columns: new[] { "DeviceTypeId", "Name" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceTypes_ParentId_Name",
+                table: "DeviceTypes",
+                columns: new[] { "ParentId", "Name" },
+                unique: true,
+                filter: "ParentId IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DevicePropertyValues");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "DeviceTypeProperties");
+
+            migrationBuilder.DropTable(
+                name: "DeviceTypes");
+        }
+    }
+}
