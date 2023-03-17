2a75ccc9ca0709bfaec545856a19c4eb4d3a1f90
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-03-17 08:00:50 +0100

    Add unit of work

---
+++ src/Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+
+namespace Infrastructure.Persistence
+{
+    internal sealed class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
