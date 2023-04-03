a037db8202913f1a732dbea90793c473470d5beb
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-04-03 08:49:53 +0200

    Add not found exception

---
+++ src/Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,25 @@
+﻿namespace Application.Common.Exceptions
+{
+    public abstract class NotFoundException : Exception
+    {
+        protected NotFoundException()
+        : base()
+        {
+        }
+
+        protected NotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        protected NotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+        }
+    }
+}
