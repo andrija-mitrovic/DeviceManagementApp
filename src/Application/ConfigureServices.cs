2e06a6d22f1e09c7056fe7e1f05533f427b68d83
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-04-07 08:47:06 +0200

    Add mapping class

--- src/Application/ConfigureServices.cs
+++ src/Application/ConfigureServices.cs
@@ -1,10 +1,14 @@
-﻿namespace Microsoft.Extensions.DependencyInjection
+﻿using FluentValidation;
+using System.Reflection;
+
+namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ConfigureServices
     {
         public static void AddApplicationServices(this IServiceCollection services)
         {
-
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
